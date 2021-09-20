cc3b702b5766cd658e942c4301aee9c33674e1e7
sukhreet028 <sukhreet028@gmail.com>
2021-09-20 17:16:59 -0400

    lastClass

--- js/gallery.js
+++ js/gallery.js
@@ -1,57 +1,57 @@
-const mainGallery = document.querySelector('.main-gallery');
-const gallery = document.querySelector('.gallery-images');
+const mainGallery = document.querySelector(".main-gallery");
+const gallery = document.querySelector(".gallery-images");
 let galleryToggle = false;
 // var normalWed= ['../images/o2.jpeg'];
 // var normalFam =['../images/o3.jpeg']
 var eventGallery = [
-  '../images/e0.jpeg',
-  '../images/e0.1.jpeg',
-  '../images/e1.jpeg',
-  '../images/e2.jpeg',
-  '../images/e3.jpeg',
-  '../images/e4.jpeg',
-  '../images/e5.jpeg',
-  '../images/e6.jpeg',
+  "../images/e0.jpeg",
+  "../images/e0.1.jpeg",
+  "../images/e1.jpeg",
+  "../images/e2.jpeg",
+  "../images/e3.jpeg",
+  "../images/e4.jpeg",
+  "../images/e5.jpeg",
+  "../images/e6.jpeg",
 ];
 var normalPics = [
-  '../images/img0.jpeg',
-  '../images/img0.1.jpeg',
-  '../images/img1.jpeg',
-  '../images/img2.jpeg',
-  '../images/img3.jpeg',
-  '../images/img4.jpeg',
-  '../images/img5.jpeg',
-  '../images/img6.jpeg',
+  "../images/img0.jpeg",
+  "../images/img0.1.jpeg",
+  "../images/img1.jpeg",
+  "../images/img2.jpeg",
+  "../images/img3.jpeg",
+  "../images/img4.jpeg",
+  "../images/img5.jpeg",
+  "../images/img6.jpeg",
 ];
 var professionalGal = [
-  '../images/p00.jpeg',
-  '../images/p0.jpeg',
-  '../images/p1.jpeg',
-  '../images/p2.jpeg',
-  '../images/p3.jpeg',
-  '../images/p4.jpeg',
-  '../images/p5.jpeg',
-  '../images/p6.jpeg',
+  "../images/p00.jpeg",
+  "../images/p0.jpeg",
+  "../images/p1.jpeg",
+  "../images/p2.jpeg",
+  "../images/p3.jpeg",
+  "../images/p4.jpeg",
+  "../images/p5.jpeg",
+  "../images/p6.jpeg",
 ];
 var natureGal = [
-  '../images/n00.jpeg',
-  '../images/n0.1.jpeg',
-  '../images/n1.jpeg',
-  '../images/n2.jpeg',
-  '../images/n3.jpeg',
-  '../images/n4.jpeg',
-  '../images/n5.jpeg',
-  '../images/n6.jpeg',
+  "../images/n00.jpeg",
+  "../images/n0.1.jpeg",
+  "../images/n1.jpeg",
+  "../images/n2.jpeg",
+  "../images/n3.jpeg",
+  "../images/n4.jpeg",
+  "../images/n5.jpeg",
+  "../images/n6.jpeg",
 ];
 var otherGal = [
-  '../images/o0.1.jpeg',
-  '../images/o0.jpeg',
-  '../images/o1.jpeg',
-  '../images/o2.jpeg',
-  '../images/o3.jpeg',
-  '../images/o4.jpeg',
-  '../images/o5.jpeg',
-  '../images/o6.jpeg',
+  "../images/o0.1.jpeg",
+  "../images/o0.jpeg",
+  "../images/o1.jpeg",
+  "../images/o2.jpeg",
+  "../images/o3.jpeg",
+  "../images/o4.jpeg",
+  "../images/o5.jpeg",
+  "../images/o6.jpeg",
 ];
 
 let imageCurrentArray = normalPics;
@@ -60,7 +60,6 @@ let imageCurrentArray = normalPics;
 window.onload = () => {
   addImagesToGallery();
 };
-
 const addImagesToGallery = () => {
   imageCurrentArray.map((data, index) => {
     if (index < 2) {
@@ -68,16 +67,18 @@ const addImagesToGallery = () => {
       console.log(img1);
       img1.style.backgroundImage = "url('" + data + "')";
     } else {
-      const imageComponent = document.createElement('img');
-      const imageDiv = document.createElement('div');
-      imageComponent.setAttribute('alt', 'image');
-      imageComponent.setAttribute('id', index);
-      imageComponent.setAttribute('src', data);
-      imageDiv.setAttribute('id', 'D' + index);
+      const imageComponent = document.createElement("img");
+      console.log("image compnent", imageComponent);
+      const imageDiv = document.createElement("div");
+      console.log("imgdiv", imageDiv);
+      imageComponent.setAttribute("alt", "image");
+      imageComponent.setAttribute("id", index);
+      imageComponent.setAttribute("src", data);
+      imageDiv.setAttribute("id", "D" + index);
       setTimeout(() => {
         document
           .getElementById(index)
-          .addEventListener('click', () => showFullSIzeImage(data, index));
+          .addEventListener("click", () => showFullSIzeImage(data, index));
       }, 1000);
       imageDiv.appendChild(imageComponent);
       gallery.appendChild(imageDiv);
@@ -85,50 +86,21 @@ const addImagesToGallery = () => {
     }
   });
 };
-// function changeText(){
-//     const msg=document.getElementById('gallery-quote');
-//     msg.innerHTML=`"Sometimes, challenges and struggles are exactly what we need in our lives...
-//     May you welcome every effort, every struggle,
-//     and every challenge...May you open your wings and fly!"`;
-// }
 
-// const selectListDiv=document.querySelector('.select-list');
-const selectList = document.getElementById('gallery');
-//   selectList.addEventListener('change',myEvent);
-
-// function myEvent(){
-//     eventGallery.map((data, index) => {
-//         console.log(data);
-//         var img1=document.getElementById(`event-images${index+1}`);
-//         console.log(img1);
-//     img1.style.backgroundImage = "url('"+data+"')";
-//     const heading=document.getElementById('gallery-heading');
-//     msg.innerHTML=`EVENT GALLERY`;
-//  })
-// }
-//  function changeText(){
-//     const heading=document.getElementById('gallery-heading');
-//     msg.innerHTML=`EVENT GALLERY`
-//     const para=document.getElementById('gallery-para');
-//     msg.innerHTML=`"Sometimes, challenges and struggles are exactly what we need in our lives...
-//     May you welcome every effort, every struggle,
-//    and every challenge...May you open your wings and fly!"`;
-// }
-// changeText();
-// }
+const selectList = document.getElementById("gallery");
 
 function showGalleryImg(galleryType) {
-  var mySecondList = document.getElementById('option-list');
+  var mySecondList = document.getElementById("option-list");
   mySecondList.style.display = null;
   galleryToggle = false;
 
-  if (galleryType === 'natureGal') {
+  if (galleryType === "natureGal") {
     showImages(natureGal);
-  } else if (galleryType === 'otherGal') {
+  } else if (galleryType === "otherGal") {
     showImages(otherGal);
-  } else if (galleryType === 'eventGal') {
+  } else if (galleryType === "eventGal") {
     showImages(eventGallery);
-  } else if (galleryType === 'proGal') {
+  } else if (galleryType === "proGal") {
     showImages(professionalGal);
   }
 }
@@ -136,7 +108,7 @@ function showGalleryImg(galleryType) {
 const showImages = (galleryType) => {
   imageCurrentArray.map((data, index) => {
     if (index > 1) {
-      const imageDiv = document.getElementById('D' + index);
+      const imageDiv = document.getElementById("D" + index);
       gallery.removeChild(imageDiv);
       if (index + 1 === imageCurrentArray.length) {
         imageCurrentArray = galleryType;
@@ -145,34 +117,53 @@ const showImages = (galleryType) => {
     }
   });
 };
+
+// let loadMoreBtn = document.getElementById("imgButton");
+// // const gallery = document.querySelector(".gallery-images");
+// loadMoreBtn.addEventListener("click", showMoreImg);
+
+// function showMoreImg(index) {
+//   for (var i = 7; i < imageCurrentArray.length; i++) {
+//     // const gallery = document.querySelector(".gallery-images");
+//     if (i < imageCurrentArray.length - 1) {
+//       var img1 = document.getElementById(`event-images${index + 1}`);
+//       console.log(img1);
+//       img1.style.backgroundImage = "url('" + data + "')";
+//     } else {
+//       i = 0;
+//     }
+//   }
+// }
+// showMoreImg();
+
 let gallerybtn = false;
-const galleryList = document.querySelector('.gallery-nav');
+const galleryList = document.querySelector(".gallery-nav");
 // const contactList=document.querySelector('.contact-nav');
 
-const galleryButton = document.querySelector('.gallery-burger-icon');
+const galleryButton = document.querySelector(".gallery-burger-icon");
 
-galleryButton.addEventListener('click', () => {
+galleryButton.addEventListener("click", () => {
   gallerybtn = !gallerybtn;
   if (gallerybtn) {
-    galleryButton.classList.add('btnClassGallery');
-    galleryList.classList.add('galleryMenu');
+    galleryButton.classList.add("btnClassGallery");
+    galleryList.classList.add("galleryMenu");
   } else {
-    galleryButton.classList.remove('btnClassGallery');
-    galleryList.classList.remove('galleryMenu');
+    galleryButton.classList.remove("btnClassGallery");
+    galleryList.classList.remove("galleryMenu");
   }
 });
 
 function myList() {
-  console.log('inside llll');
-  var mySecondList = document.querySelector('.gallery-dropdown');
-  mySecondList.classList.add('myNewList');
+  console.log("inside llll");
+  var mySecondList = document.querySelector(".gallery-dropdown");
+  mySecondList.classList.add("myNewList");
 }
 const showGalleryOptions = () => {
-  console.log('inside');
+  console.log("inside");
   galleryToggle = !galleryToggle;
-  var mySecondList = document.getElementById('option-list');
+  var mySecondList = document.getElementById("option-list");
   if (galleryToggle) {
-    mySecondList.style.display = 'block';
+    mySecondList.style.display = "block";
   } else {
     mySecondList.style.display = null;
   }
@@ -182,19 +173,19 @@ let previousImage;
 const showFullSIzeImage = (data, id) => {
   currentLargeImage = id;
   previousImage = id;
-  document.getElementById('gallery-container').style.display = 'none';
-  const largeImg = document.getElementById('main-image');
-  const largeImgWrapper = document.getElementById('main-image-wrapper');
-  largeImg.setAttribute('src', data);
-  largeImgWrapper.style.display = 'flex';
+  document.getElementById("gallery-container").style.display = "none";
+  const largeImg = document.getElementById("main-image");
+  const largeImgWrapper = document.getElementById("main-image-wrapper");
+  largeImg.setAttribute("src", data);
+  largeImgWrapper.style.display = "flex";
 };
 
 const showGallery = () => {
-  const prevImage = document.getElementById('moveImage');
-  prevImage.setAttribute('href', '#D' + previousImage);
+  const prevImage = document.getElementById("moveImage");
+  prevImage.setAttribute("href", "#D" + previousImage);
   prevImage.click();
-  const largeImgWrapper = document.getElementById('main-image-wrapper');
-  document.getElementById('gallery-container').style.display = null;
+  const largeImgWrapper = document.getElementById("main-image-wrapper");
+  document.getElementById("gallery-container").style.display = null;
   largeImgWrapper.style.display = null;
 
   setTimeout(() => {
@@ -203,31 +194,31 @@ const showGallery = () => {
 };
 
 const leftMove = () => {
-  const largeImg = document.getElementById('main-image');
+  const largeImg = document.getElementById("main-image");
   if (currentLargeImage <= 2) {
-    largeImg.setAttribute('src', imageCurrentArray[currentLargeImage]);
+    largeImg.setAttribute("src", imageCurrentArray[currentLargeImage]);
   } else {
-    largeImg.setAttribute('src', imageCurrentArray[currentLargeImage - 1]);
+    largeImg.setAttribute("src", imageCurrentArray[currentLargeImage - 1]);
     currentLargeImage = currentLargeImage - 1;
   }
 };
 
 const rightMove = () => {
-  const largeImg = document.getElementById('main-image');
+  const largeImg = document.getElementById("main-image");
   if (currentLargeImage + 1 === imageCurrentArray.length) {
-    largeImg.setAttribute('src', imageCurrentArray[currentLargeImage]);
+    largeImg.setAttribute("src", imageCurrentArray[currentLargeImage]);
   } else {
-    largeImg.setAttribute('src', imageCurrentArray[currentLargeImage + 1]);
+    largeImg.setAttribute("src", imageCurrentArray[currentLargeImage + 1]);
     currentLargeImage = currentLargeImage + 1;
   }
 };
 
-//  function showList(index){
-//     var galleryList1= document.querySelector('.gallery-dropdown');
-//     console.log(galleryList1);
-//     var galleryItems= galleryList1.querySelectorAll('li');
-//     console.log(galleryItems);
-//           var item2= galleryItems.value;
-//              console.log(item2);
-//     }
-//  showList();
+function showList(index) {
+  var galleryList1 = document.querySelector(".gallery-dropdown");
+  console.log(galleryList1);
+  var galleryItems = galleryList1.querySelectorAll("li");
+  console.log(galleryItems);
+  var item2 = galleryItems.value;
+  console.log(item2);
+}
+showList();

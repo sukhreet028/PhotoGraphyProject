2109c230b11dec5662e847dd156f25adafadd2e1
Sukhvir2104 <sukhvir21@live.com>
2021-10-04 18:38:14 -0400

    fix the scroll bug in conatct and about page and minor changes

--- html/slider1.js
+++ html/slider1.js
@@ -1,40 +1,57 @@
 var i = 0;
+let deviceWidth = window.innerWidth;
+window.onresize = () => {
+  deviceWidth = window.innerWidth;
+  console.log('inside func ' + deviceWidth);
+};
+
+console.log('outside func ' + deviceWidth);
 var sliderState = true;
 var imgArray = [
-  "../images/hp10.jpeg",
-  "../images/hp2.jpeg",
-  "../images/hp11.jpeg",
-  "../images/hp9.jpeg",
-  "../images/hp7.jpeg",
+  '../images/hp10.jpeg',
+  '../images/hp2.jpeg',
+  '../images/hp11.jpeg',
+  '../images/hp9.jpeg',
+  '../images/hp7.jpeg',
 ];
+// logic to change the images on mobile and ipad 
+// if (deviceWidth < 576) {
+//   imgArray = [
+
+//   ]
+// } else if(deviceWidth < 900) {
+//   imgArray = [
+
+//   ]
+// }
 var j = 0;
 var imgArray1 = [
   // "../images/a3.jpeg",
-  "../images/a4.jpeg",
-  "../images/a5.jpeg",
-  "../images/a9.jpeg",
+  '../images/a4.jpeg',
+  '../images/a5.jpeg',
+  '../images/a9.jpeg',
 ];
 function slider() {
-  const image1 = document.getElementById("homeImage");
-  const image2 = document.getElementById("homeImage2");
+  const image1 = document.getElementById('homeImage');
+  const image2 = document.getElementById('homeImage2');
   if (i === 0 && sliderState) {
-    image1.setAttribute("src", imgArray[i]);
-    image2.style.display = "none";
+    image1.setAttribute('src', imgArray[i]);
+    image2.style.display = 'none';
   } else {
     image2.style.display = null;
     sliderState = false;
     if (i % 2 === 0) {
-      image1.classList.remove("goOut");
-      image2.classList.remove("comeIn");
-      image2.classList.add("goOut");
-      image1.classList.add("comeIn");
-      image1.setAttribute("src", imgArray[i]);
+      image1.classList.remove('goOut');
+      image2.classList.remove('comeIn');
+      image2.classList.add('goOut');
+      image1.classList.add('comeIn');
+      image1.setAttribute('src', imgArray[i]);
     } else {
-      image2.classList.remove("goOut");
-      image1.classList.remove("comeIn");
-      image1.classList.add("goOut");
-      image2.classList.add("comeIn");
-      image2.setAttribute("src", imgArray[i]);
+      image2.classList.remove('goOut');
+      image1.classList.remove('comeIn');
+      image1.classList.add('goOut');
+      image2.classList.add('comeIn');
+      image2.setAttribute('src', imgArray[i]);
     }
   }
 
@@ -47,7 +64,7 @@ function slider() {
 // setTimeout("slider()",1500);
 
 function slider1() {
-  document.getElementById("awardImg1").setAttribute("src", imgArray1[j]);
+  document.getElementById('awardImg1').setAttribute('src', imgArray1[j]);
   if (j < imgArray1.length - 1) {
     j++;
   } else {
@@ -65,21 +82,16 @@ function bothSlider() {
 }
 window.onload = bothSlider;
 
-// function myList(){
-//     var mainList=document.getElementById('myMainList');
-//     mainList.classList.add('main-nav-new');
-// }
-
 let burgerIcon = false;
-const burgerBtn = document.querySelector(".burger-icon");
-const navList = document.querySelector(".home-nav-list");
-burgerBtn.addEventListener("click", () => {
+const burgerBtn = document.querySelector('.burger-icon');
+const navList = document.querySelector('.home-nav-list');
+burgerBtn.addEventListener('click', () => {
   burgerIcon = !burgerIcon;
   if (burgerIcon) {
-    burgerBtn.classList.add("open");
-    navList.classList.add("menu");
+    burgerBtn.classList.add('open');
+    navList.classList.add('menu');
   } else {
-    burgerBtn.classList.remove("open");
-    navList.classList.remove("menu");
+    burgerBtn.classList.remove('open');
+    navList.classList.remove('menu');
   }
 });
